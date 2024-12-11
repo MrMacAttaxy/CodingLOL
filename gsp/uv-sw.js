dc08094300330492e77f142e2c003e20ab8aaf34
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2024-12-10 21:04:56 -0500

    Delete gsp directory

--- gsp/uv-sw.js
+++
@@ -1,5 +0,0 @@
-importScripts("https://codingcopyandpaste.mywire.org/gsp/uv/uv.sw.js");
-
-const sw = new UVServiceWorker();
-
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
--- gsp/uv.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://codingcopyandpaste.mywire.org/gps//uv/uv.bundle.js');
-importScripts('https://codingcopyandpaste.mywire.org/gps//uv/uv.config.js');
-importScripts('https://codingcopyandpaste.mywire.org/gps//uv/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
